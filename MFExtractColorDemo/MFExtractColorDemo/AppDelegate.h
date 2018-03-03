617cb28778e86d1002eb9bb490f761c9a570de9f
GodzzZZZ <GodzzZZZ@qq.com>
2018-03-03 11:09:27 +0800

    Initial commit

---
+++ MFExtractColorDemo/MFExtractColorDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MFExtractColorDemo
+//
+//  Created by pipelining on 2018/3/3.
+//  Copyright © 2018年 GodzzZZZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MFExtractColorDemo/MFExtractColorDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MFExtractColorDemo
+//
+//  Created by pipelining on 2018/3/3.
+//  Copyright © 2018年 GodzzZZZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
